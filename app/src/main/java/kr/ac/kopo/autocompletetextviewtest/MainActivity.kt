ac08313d0190b7576ada473c2fc620b602abbdb8
rrvv123 <eorms7391@gmail.com>
2024-04-25 15:10:34 +0900

    0425 - 7교시 15:10 중 자동과 멀티자동 완성 텍스트뷰

---
+++ app/src/main/java/kr/ac/kopo/autocompletetextviewtest/MainActivity.kt
@@ -0,0 +1,29 @@
+package kr.ac.kopo.autocompletetextviewtest
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.MultiAutoCompleteTextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var items = arrayOf("apple", "appstore", "application", "banana", "band", "bard", "friend", "fruit")
+
+        var adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items)
+
+        var auto = findViewById<AutoCompleteTextView>(R.id.auto)
+        auto.setAdapter(adapter)
+
+        var multiAuto = findViewById<MultiAutoCompleteTextView>(R.id.multiAuto)
+        var token = MultiAutoCompleteTextView.CommaTokenizer()
+        multiAuto.setTokenizer(token)
+        multiAuto.setAdapter(adapter)
+    }
+}
